df1030f99ef5efa0e75061899854a90bbfefc165
NawalMalki <malkinawal2004@gmail.com>
2023-11-27 19:36:20 +0100

    Initial commit

--- src/components/AboutHero.js
+++ src/components/AboutHero.js
@@ -1,9 +1,17 @@
-import "./HeroStyles.css"
+import "./AboutHeroStyles.css"
 
 
 
-export default function AboutHero(){
+export default function AboutHero(props){
     return(
-        <div>Hero section</div>
+        <div className="props.cName">
+            <img alt="HeroImg" src={props.heroImg}/>
+            <div className="hero-text">
+                <h1>{props.title}</h1>
+                <p>{props.paragraph}</p>
+                
+            </div>
+
+        </div>
     )
 }
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,12 +1,17 @@
 import AboutHero from '../components/AboutHero';
 import AboutUs from '../components/AboutUs';
 import Footer from '../components/Footer';
-import Navbar from '../components/Navbar'
+import Navbar from '../components/Navbar';
+import AboutImg from '../images/AboutImg.jpg'
 function About(){
     return(
         <>
         <Navbar />
-        <AboutHero />
+        <AboutHero 
+        cName=".hero"
+        heroImg={AboutImg}
+        title="About"
+        />
         <AboutUs />
         <Footer />
         
